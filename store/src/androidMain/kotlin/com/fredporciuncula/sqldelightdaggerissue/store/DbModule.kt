2660a0b6b123bc110a7e6cdb165ddb6cac70869d
Fred Porciúncula <tfcporciuncula@gmail.com>
2021-11-16 17:07:52 +0100

    Add comment on how to induce error

--- store/src/androidMain/kotlin/com/fredporciuncula/sqldelightdaggerissue/store/DbModule.kt
+++ store/src/androidMain/kotlin/com/fredporciuncula/sqldelightdaggerissue/store/DbModule.kt
@@ -13,4 +13,16 @@ class DbModule {
     fun provideDatabase(context: Context): MyDatabase {
         return MyDatabase(AndroidSqliteDriver(MyDatabase.Schema, context, "test.db"))
     }
+
+    @Provides
+    fun provideQueries1(database: MyDatabase): Queries1Queries {
+        return database.queries1Queries
+    }
+
+    // To induce the error, create a new `Queries2.sq` file, uncomment this provides, and build the project.
+    // You can keep adding new files and new provides methods to keep bringing the error back.
+//    @Provides
+//    fun provideQueries2(database: MyDatabase): Queries2Queries {
+//        return database.queries2Queries
+//    }
 }
